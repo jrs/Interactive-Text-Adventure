5069705bd6d40ffd9c0e43f15616d8c425b1849e
Justin Scott <hello@jrs.dev>
2022-11-28 10:15:46 -0800

    updating

--- project-3/Assets/Scripts/SceneLoader.cs
+++ project-3/Assets/Scripts/SceneLoader.cs
@@ -16,6 +16,11 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(0);
    }
 
+   public void LoadSceneNameHereScene()
+   {
+     SceneManager.LoadScene(10);
+   }
+
    public void QuitGame()
    {
         Application.Quit();
